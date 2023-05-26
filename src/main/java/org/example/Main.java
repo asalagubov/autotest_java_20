74d009250c225b82bbf65de46793f42042dc5ecc
Aleksandr Salagubov <salagubov@icloud.com>
2023-05-26 12:15:10 +0300

    var added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Sasha !!!");
+        String message = "Hello, Sasha !";
+        System.out.println("Hello, Sasha !");
     }
 }
