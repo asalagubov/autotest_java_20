d82d3ce52be9ac60d86e8e5d7cca628306d4052c
Aleksandr Salagubov <salagubov@icloud.com>
2023-05-26 11:58:31 +0300

    change messege

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Sasha!");
+        System.out.println("Hello, Sasha !!!");
     }
 }
