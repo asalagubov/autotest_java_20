dbacab20782ccca064cb7f3aefdb957024c81b71
Aleksandr Salagubov <105970564+asalagubov@users.noreply.github.com>
2023-05-30 14:29:44 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<<<< Temporary merge branch 1
         if (args.length == 0) {
             System.out.println("Hello, Sasha!");
         }
-=========
         String message = "Hello, Sasha !";
         System.out.println("Hello, Sasha !");
->>>>>>>>> Temporary merge branch 2
+
     }
 }
