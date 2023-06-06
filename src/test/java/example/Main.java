5b60d81365d45fe91ad31f19ef9fe4a3fb5abdd0
Aleksandr Salagubov <salagubov@icloud.com>
2023-06-06 23:13:01 +0300

    fix

--- src/test/java/example/Main.java
+++ src/test/java/example/Main.java
@@ -1,14 +1,11 @@
-package org.example;
+package example;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<<<< Temporary merge branch 1
         if (args.length == 0) {
             System.out.println("Hello, Sasha!");
         }
-=========
         String message = "Hello, Sasha !";
         System.out.println("Hello, Sasha !");
->>>>>>>>> Temporary merge branch 2
     }
 }
